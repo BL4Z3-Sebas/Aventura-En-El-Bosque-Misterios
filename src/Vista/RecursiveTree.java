16ccb82ee2977ca5696672ab6bb9def67a69729b
adcohen1 <adcohen@uninorte.edu.co>
2024-09-24 06:12:50 -0500

    mapa agregado

--- src/Vista/RecursiveTree.java
+++ src/Vista/RecursiveTree.java
@@ -4,16 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
 
-public class RecursiveTree extends javax.swing.JPanel {
+public class RecursiveTree extends JPanel {
+    
+    private static final int MAX_R = 17;
+    private static final int MAX_COLOR_VALUE = 255;
+    private static final int INITIAL_BRANCH_LENGTH = 260;
+    
+    private float theta;
+    private float a = 0;
+    private float r = 0;
+    private static Timer timer;
 
-    float theta;
-    float a = 0;
-    float r = 0;
-    static Timer timer;
-
-    float velocidadMaxima = 1.5f; // Velocidad máxima que queremos alcanzar
-    float velocidadActual = 0.0001f; // Velocidad inicial lenta
-    float aceleracion = 0.001f; // Aceleración que incrementa la velocidad
+    private final float velocidadMaxima = 1.5f; // Velocidad máxima que queremos alcanzar
+    private float velocidadActual = 0.0001f; // Velocidad inicial lenta
+    private final float aceleracion = 0.001f; // Aceleración que incrementa la velocidad
 
     public RecursiveTree() {
         setBackground(Color.BLACK);
@@ -26,19 +30,35 @@ public class RecursiveTree extends javax.swing.JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        // Calcular el color actual basado en el progreso de r
-        // r varia de 0 a 30, así que usamos un rango de 0 a 255 para el color
-        int colorValue = (int) (125 + r / 17 * 130);  // Interpolación lineal de 0 (oscuro) a 255 (blanco)
-        colorValue = Math.min(colorValue, 255); // Aseguramos que no pase de 255
-        g2d.setColor(new Color(colorValue, colorValue, colorValue)); // Color de gris oscuro a blanco
+        // Calcular el color actual
+        int colorValue = calcularColor();
+        g2d.setColor(new Color(colorValue, colorValue, colorValue));
 
-        if (r < 17) {
+        actualizarParametros();
+
+        theta = (float) Math.toRadians(a);
+        AffineTransform old = g2d.getTransform();
+        g2d.rotate(Math.toRadians(90), getWidth() / 2, getHeight() / 2);
+        g2d.translate(getWidth() / 2, getHeight());
+
+        g2d.drawLine(0, 100, 0, -INITIAL_BRANCH_LENGTH);
+        g2d.translate(0, -INITIAL_BRANCH_LENGTH);
+
+        branch(g2d, getHeight() / 2.5f);
+
+        g2d.setTransform(old);
+    }
+
+    private int calcularColor() {
+        int colorValue = (int) (125 + r / 17 * 130);  // Interpolación lineal de 0 a 255
+        return Math.min(colorValue, MAX_COLOR_VALUE); // Aseguramos que no pase de 255
+    }
+
+    private void actualizarParametros() {
+        if (r < MAX_R) {
             r += velocidadActual; // Aumentar r usando la velocidad actual
             a += 0.03 - 0.01 * r + 0.002 * Math.pow(r, 2);
 
-//            if (a > 17) {
-//                a = 17;
-//            }
             // Incrementar la velocidad actual con aceleración, pero no exceder la velocidad máxima
             if (velocidadActual < velocidadMaxima) {
                 velocidadActual += aceleracion;
@@ -46,23 +66,6 @@ public class RecursiveTree extends javax.swing.JPanel {
         } else {
             timer.stop();
         }
-
-        theta = (float) Math.toRadians(a);
-
-        AffineTransform old = g2d.getTransform();
-        g2d.rotate(Math.toRadians(90), getWidth() / 2, getHeight() / 2);
-        g2d.translate(getWidth() / 2, getHeight());
-
-        g2d.drawLine(0, 100, 0, -260);
-        g2d.translate(0, -260);
-
-        branch(g2d, getHeight() / 2.5f);
-
-        g2d.setTransform(old);
-    }
-
-    public static void reiniciar() {
-        timer.restart();
     }
 
     private void branch(Graphics2D g2d, float h) {
